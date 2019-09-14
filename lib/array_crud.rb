03d33d2814ea1ebd72ff0db6616dfd472dfdb5f6
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-14 00:42:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array3 = ["omg","really?"]
+  array3.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)

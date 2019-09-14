09c71ef38bc40c9125034c57c818c404e360b74c
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-14 00:41:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array_2 = ["b","r","u","h"]
-  array_2.push ["arrays!"]
+  array_2.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
